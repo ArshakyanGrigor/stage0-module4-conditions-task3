6fb12b6e0e38d97b1a435b9a1c0dd2a5dbe24352
ArshakyanGrigor <grigor.arshakyan1994@mail.ru>
2022-12-17 00:19:33 +0400

    Updated

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,21 +2,21 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double calculatedSalary = 0;
         if (salary <= 0) {
             System.out.println("wrong input!");
         }
         if(salary > 0 && salary <= 10000){
-            salary = salary - (salary * 15 / 100);
-            System.out.println(salary);
+            calculatedSalary = salary - (salary * 15 / 100);
+            System.out.println(calculatedSalary);
         }
         if(salary > 10000 && salary<=20000){
-            salary = salary - (salary * 18 / 100);
-            System.out.println(salary);
+            calculatedSalary = salary - (salary * 18 / 100);
+            System.out.println(calculatedSalary);
         }
-
         if(salary > 20000){
-            salary = salary - (salary * 20 / 100);
-            System.out.println(salary);
+            calculatedSalary = salary - (salary * 20 / 100);
+            System.out.println(calculatedSalary);
         }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,22 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-        switch (month) {
-            case 1, 2, 12 -> {
-                System.out.println("winter");
-            }
-            case 3, 4, 5 -> {
-                System.out.println("spring");
-            }
-            case 6, 8, 7 -> {
-                System.out.println("summer");
-            }
-            case 9, 10, 11 -> {
-                System.out.println("autumn");
-            }
-            default -> {
-                System.out.println("wrong number!");
-            }
+        if (month == 1 || month == 2 || month == 12) {
+            System.out.println("Winter");
+        } else if (month >= 3 && month <= 5) {
+            System.out.println("Spring");
+        } else if (month >= 6 && month <= 9) {
+            System.out.println("Summer");
+        } else if (month >= 9 && month <= 11) {
+            System.out.println("Autumn");
+        } else {
+            System.out.println("wrong number!");
         }
     }
 }
